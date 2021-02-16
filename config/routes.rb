6da395c9b0625cf104c42f5a545af75b46389584
Stefan Koch <webokoch@gmx.de>
2021-02-16 13:42:56 +0100

    create action added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'tasks', to: 'tasks#index'
+  get 'task/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
