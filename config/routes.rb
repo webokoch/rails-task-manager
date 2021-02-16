7844881791386a9000841f9b7f5b286e97dcf53f
Stefan Koch <webokoch@gmx.de>
2021-02-16 13:59:23 +0100

    update action added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'task/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
