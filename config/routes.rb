82487906fc36af3912806b63c2d1639beaaa1543
Stefan Koch <webokoch@gmx.de>
2021-02-16 12:35:14 +0100

    actions show and index added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
